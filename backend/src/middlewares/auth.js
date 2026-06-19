443e13cbce89ceb5d0d3df1f3be43e6d24436293
ThurrHenrick062 <arthurhenrick062@gmail.com>
2026-06-18 22:13:51 -0300

    Criação: confirmação do token

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+export function auth (req, res, next){
+    const authHeader = req.header['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({message: 'Token não fornecido'})
+    }
+
+    try{
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = payload
+        next()
+    } catch (err){
+        return res.status(403).json({message: "Token inspirado ou invalido"})
+    }
+
+}
