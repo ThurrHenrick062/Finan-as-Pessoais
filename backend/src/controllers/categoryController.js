a9f363da272fb9eccea1f6abcc7312dfd09006ad
ThurrHenrick062 <arthurhenrick062@gmail.com>
2026-06-21 10:13:25 -0300

    Criação: Controller das catagorias criado

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -23,3 +23,26 @@ export async function criar(req,res) {
     res.status(201).json({message: 'Categoria criada'})
     
 }
+
+export async function atualizar(req,res) {
+    const id = req.params.id
+    const {name, type} = req.body
+    const userId = req.user.id
+
+    const resposta = await Categories.update(id,userId,name,type)
+    if(resposta == 0){
+        return res.status(404).json({message: "Categoria não encontrada"})
+    }
+    return res.status(200).json({message: "Categoria atualizada com sucesso"})
+}
+
+export async function deletar(req, res) {
+    const id = req.params.id
+    const userId = req.user.id
+
+    const resposta = await Categories.del(id,userId)
+    if(resposta == 0){
+        return res.status(404).json({message: "Categoria não encontrada"})
+    }
+    return res.status(200).json({message: "Categoria excluida com sucesso"})
+}
