7895a04e1c46a8367c0b64624acef67f6eb039dd
ThurrHenrick062 <arthurhenrick062@gmail.com>
2026-06-25 21:02:09 -0300

    Criação: Model transação delete

--- backend/src/models/transactionModel.js
+++ backend/src/models/transactionModel.js
@@ -2,33 +2,41 @@ import pool from "../config/db.js";
 
 export async function getAll(userId) {
     const [result] = await pool.query(
-        "SELECT * FROM trasaction WHERE user_id = ?",[userId]
+        "SELECT * FROM transactions WHERE user_id = ?",[userId]
     )
     return result    
 }
 
 export async function getById(id,userId) {
     const [result] = await pool.query(
-        "SELECT * FROM trasaction WHERE user_id = ? AND id = ?",[userId, id]
+        "SELECT * FROM transactions WHERE user_id = ? AND id = ?",[userId, id]
     )
-    return result
+    return result[0]
 }
 
-export async function create(id,userId,categoryId,description,amount,type,date) {
+export async function create(userId,categoryId,description,amount,type,date) {
     const [result] = await pool.query(
-        "INSERT INTO transaction (id,user_id,category_id,description,amount,type,date) VALUES (?,?,?,?,?,?,?)", [id,userId,categoryId,description,amount,type,date])
+      "INSERT INTO transactions (user_id,category_id,description,amount,type,date) VALUES (?,?,?,?,?,?)",
+      [userId, categoryId, description, amount, type, date]);
         return result.insertId
     
 }
 
 export async function update(id,userId,categoryId,description,amount,type,date) {
     const [result] = await pool.query(
-        "UPDATE transaction SET description = ?,amount = ?, type = ?, date = ? WHERE id = ? AND userId = ? AND categoryId = ?",
-        [description, amount, type, date, id, userId, categoryId]
-    )
+      "UPDATE transactions SET category_id = ?, description = ?, amount = ?, type = ?, date = ? WHERE id = ? AND user_id = ?",
+      [categoryId, description, amount, type, date, id, userId]
+    );
     return result.affectedRows
     
 }
+
+export async function del(id, userId) {
+    const [result] = await pool.query(
+        "DELETE FROM transactions WHERE id = ? AND user_id = ?",[id,userId]
+    )
+    return result.affectedRows
+}   
 
 
 
